c3690cd48bd1b94a1d467da396c5dbdee8a83eb1
yzyDavid <yzyDavid@qq.com>
2017-10-08 00:22:47 +0800

    enabled anko-{commons, sqlite}.

--- app/src/main/java/com/example/yzy/imageartist/MainActivity.kt
+++ app/src/main/java/com/example/yzy/imageartist/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.yzy.imageartist
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        (findViewById(R.id.hello_text) as TextView).apply {
+            text = "Hello hahaha"
+            toast("Hello")
+        }
     }
 }
