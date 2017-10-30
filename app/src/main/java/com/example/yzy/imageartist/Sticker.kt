76f8daf23fd573809a699147a29dcf459097d1a0
wangtianyuanpanda <973920187@qq.com>
2017-10-30 19:44:06 +0800

    titles changed by wty

--- app/src/main/java/com/example/yzy/imageartist/Sticker.kt
+++ app/src/main/java/com/example/yzy/imageartist/Sticker.kt
@@ -8,5 +8,6 @@ class Sticker : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sticker)
+        title="贴纸"
     }
 }
--- app/src/main/java/com/example/yzy/imageartist/Stylize.kt
+++ app/src/main/java/com/example/yzy/imageartist/Stylize.kt
@@ -3,11 +3,11 @@ package com.example.yzy.imageartist
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class Joint : AppCompatActivity() {
+class Stylize : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_joint)
-        title="拼图"
+        setContentView(R.layout.activity_stylize)
+        title="风格化"
     }
 }
