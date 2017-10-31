09b836a9a869e7f70120f881554827d73c068227
yzyDavid <yzyDavid@qq.com>
2017-10-31 23:10:14 +0800

    finished CameraModel, without testing.

--- app/src/main/java/com/example/yzy/imageartist/CameraModel.kt
+++ app/src/main/java/com/example/yzy/imageartist/CameraModel.kt
@@ -1,17 +1,41 @@
 package com.example.yzy.imageartist
 
 import android.app.Activity
-import android.content.Context
+import android.support.v7.app.AppCompatActivity
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.os.Environment
+import android.provider.MediaStore
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import android.support.v4.content.FileProvider
+import java.util.*
 
-class CameraModel {
-    fun bitmap(resultCode: Int, data: Intent): Bitmap {
+class CameraModel(private val activity: AppCompatActivity, private val requestCode: Int) {
+    private var photoPath: String = ""
+    fun getBitmap(resultCode: Int, data: Intent): Bitmap {
         if (resultCode != Activity.RESULT_OK) {
             throw RuntimeException("Not a valid result")
         }
-        val bitmap = BitmapFactory.decodeFile("") // FIXME
-        return bitmap
+        return BitmapFactory.decodeFile(photoPath)
+    }
+
+    @Throws(IOException::class)
+    fun startCamera() {
+        val storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+        val timeStamp = SimpleDateFormat("""yyyyMMdd_HHmmss""", Locale.CHINA)
+        val fileName = "ImageArtist_" + timeStamp
+        val image = File.createTempFile(fileName, ".jpg", storageDir)
+        photoPath = image.absolutePath
+
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (intent.resolveActivity(activity.packageManager) == null) {
+            throw RuntimeException("NO camera found")
+        }
+        val photoUri = FileProvider.getUriForFile(activity, "com.example.android.fileprovider", image)
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri)
+        activity.startActivityForResult(intent, requestCode)
     }
 }
