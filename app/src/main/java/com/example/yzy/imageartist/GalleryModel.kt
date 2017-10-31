a015fb910641f7ba773633990a7045a03d43538b
yzyDavid <yzyDavid@qq.com>
2017-10-31 23:59:56 +0800

    added GalleryPicker without checking.

--- app/src/main/java/com/example/yzy/imageartist/GalleryModel.kt
+++ app/src/main/java/com/example/yzy/imageartist/GalleryModel.kt
@@ -1,7 +1,31 @@
 package com.example.yzy.imageartist
 
-/**
- * Created by yzy on 10/31/17.
- */
-class GalleryModel {
+import android.app.Activity
+import android.content.Intent
+import android.database.Cursor
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.provider.MediaStore
+import android.support.v7.app.AppCompatActivity
+
+class GalleryModel(private val activity: AppCompatActivity, private val requestCode: Int) {
+    fun getBitmap(resultCode: Int, data: Intent): Bitmap {
+        if (resultCode != Activity.RESULT_OK) {
+            throw RuntimeException("Not a valid result")
+        }
+        val selectedImage = data.data
+        val filePathColumns = arrayOf(MediaStore.Images.Media.DATA)
+        val c = activity.contentResolver.query(selectedImage, filePathColumns, null, null, null)
+        c.moveToFirst()
+        val columnIndex = c.getColumnIndex(filePathColumns[0])
+        val picturePath = c.getString(columnIndex)
+        c.close()
+        return BitmapFactory.decodeFile(picturePath)
+    }
+
+    fun startGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        activity.startActivityForResult(intent, requestCode)
+    }
 }
