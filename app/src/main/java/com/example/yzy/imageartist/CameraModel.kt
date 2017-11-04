3babb012dc8f6956cd562d44c7ffa15a33216ca7
poiiiicen <poiiiicen@gmail.com>
2017-11-04 18:52:41 +0800

    Fix the permission error, but should be written in asynchronous

--- app/src/main/java/com/example/yzy/imageartist/CameraModel.kt
+++ app/src/main/java/com/example/yzy/imageartist/CameraModel.kt
@@ -1,17 +1,20 @@
 package com.example.yzy.imageartist
 
+import android.Manifest
 import android.app.Activity
 import android.support.v7.app.AppCompatActivity
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Environment
 import android.provider.MediaStore
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import java.io.File
 import java.io.IOException
 import java.text.SimpleDateFormat
 import android.support.v4.content.FileProvider
-import java.util.*
 
 class CameraModel(private val activity: AppCompatActivity, private val requestCode: Int) {
     private var photoPath: String = ""
@@ -26,6 +29,21 @@ class CameraModel(private val activity: AppCompatActivity, private val requestCo
     fun startCamera() {
         val storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
         val fileName = "ImageArtist_" + System.currentTimeMillis()
+        if (ContextCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                throw RuntimeException("User denies the permission before")
+            } else {
+                ActivityCompat.requestPermissions(activity, Array(1){Manifest.permission.WRITE_EXTERNAL_STORAGE}, requestCode)
+            }
+        }
+        if (ContextCompat.checkSelfPermission(activity, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(activity, Manifest.permission.CAMERA)) {
+                throw RuntimeException("User denies the permission before")
+            } else {
+                ActivityCompat.requestPermissions(activity, Array(1){Manifest.permission.CAMERA}, requestCode)
+            }
+        }
+
         val image = File.createTempFile(fileName, ".jpg", storageDir)
         photoPath = image.absolutePath
 
@@ -33,7 +51,7 @@ class CameraModel(private val activity: AppCompatActivity, private val requestCo
         if (intent.resolveActivity(activity.packageManager) == null) {
             throw RuntimeException("NO camera found")
         }
-        val photoUri = FileProvider.getUriForFile(activity, "com.example.android.fileprovider", image)
+        val photoUri = FileProvider.getUriForFile(activity, "com.example.yzy.imageartist", image)
         intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri)
         activity.startActivityForResult(intent, requestCode)
     }
