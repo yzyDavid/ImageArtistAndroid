84103ab223c1429fccbe2506aa8d1ca4d3ce0729
wangtianyuanpanda <973920187@qq.com>
2017-11-04 19:37:18 +0800

    made a singleton of workspace manager.

---
+++ app/src/main/java/com/example/yzy/imageartist/WorkspaceManager.kt
@@ -0,0 +1,10 @@
+package com.example.yzy.imageartist
+
+import android.graphics.Bitmap
+
+/**
+ * Created by wangtianyuan on 2017/11/4.
+ */
+object WorkspaceManager {
+    public var bitmap: Bitmap? = null
+}
