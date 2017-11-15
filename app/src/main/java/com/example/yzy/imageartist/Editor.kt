0f9a38439d750a998c048406e9f79a4a2c2adbe4
wangtianyuanpanda <973920187@qq.com>
2017-11-15 21:11:07 +0800

    outline redesigned by wty

--- app/src/main/java/com/example/yzy/imageartist/Editor.kt
+++ app/src/main/java/com/example/yzy/imageartist/Editor.kt
@@ -1,6 +1,7 @@
 package com.example.yzy.imageartist
 
 import android.app.Dialog
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.*
@@ -16,9 +17,15 @@ class Editor : AppCompatActivity () ,Formatter{
 
     private lateinit var mNumPicker: NumberPicker
     private lateinit var mChooseColorNum: TextView
+    private lateinit var mStylizeText: TextView
+    private lateinit var mImportText: TextView
     private lateinit var inflate: View
     private lateinit var mPhoto: ImageView
     private lateinit var mDialog: Dialog
+    private lateinit var mToolText: TextView
+    private lateinit var mColorText: TextView
+    private lateinit var mFrameText: TextView
+    private lateinit var mModifyText: TextView
     private lateinit var mProgressBar: ProgressBar
     private  var mColorNum: Int = 1
 
@@ -26,15 +33,54 @@ class Editor : AppCompatActivity () ,Formatter{
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_editor)
         this.setTitle(R.string.Editor)
-        mChooseColorNum = findViewById(R.id.choosenum_text)
+       // mChooseColorNum = findViewById(R.id.choosenum_text)
         mPhoto = findViewById(R.id.photo)
-
+        mStylizeText = findViewById(R.id.stylize_text)
+        mToolText = findViewById(R.id.tool_text)
+        mImportText = findViewById(R.id.import_text)
         WorkspaceManager.bitmap?.let {
             mPhoto.setImageBitmap(it)
         }
+        mStylizeText.setOnClickListener{
+            var intent = Intent(this,Stylize::class.java)
+            startActivity(intent)
+        }
+        mImportText.setOnClickListener{
+            var intent = Intent(this,Import::class.java)
+            startActivity(intent)
+        }
 
     }
-
+    public fun show(view: View){
+        mDialog = Dialog(this, R.style.ActionSheetDialogAnimation)
+        inflate = LayoutInflater.from(this).inflate(R.layout.tool_selection, null)
+        mColorText = inflate.findViewById(R.id.color_text)
+        mFrameText = inflate.findViewById(R.id.frame_text)
+        mModifyText = inflate.findViewById(R.id.modify_text)
+        mDialog.setContentView(inflate)
+        val dialogWindow: Window = mDialog.window
+        dialogWindow.setGravity(Gravity.CENTER)
+        val lp: WindowManager.LayoutParams = dialogWindow.attributes
+        lp.alpha=9f
+        inflate.measure(0, 0)
+        lp.height = inflate.measuredHeight
+        lp.width = inflate.measuredWidth
+        dialogWindow.attributes = lp
+        mDialog.show()
+        mColorText.setOnClickListener{
+            var intent = Intent(this,Color::class.java)
+            startActivity(intent)
+        }
+        mFrameText.setOnClickListener{
+            var intent = Intent(this,Frame::class.java)
+            startActivity(intent)
+        }
+        mModifyText.setOnClickListener{
+            var intent = Intent(this,Modify::class.java)
+            startActivity(intent)
+        }
+    }
+/*
     public fun show(view: View) {
         mDialog = Dialog(this, R.style.ActionSheetDialogAnimation)
         inflate = LayoutInflater.from(this).inflate(R.layout.choosecolornum, null)
@@ -72,7 +118,7 @@ class Editor : AppCompatActivity () ,Formatter{
             mDialog.show()
         }
     }
-
+*/
     override fun format(value: Int): String {
         var tmpStr = value.toString()
         if (value < 10) {
