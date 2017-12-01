6fe0672622243dd462bb1a75e23e131aa66e6266
wangtianyuanpanda <973920187@qq.com>
2017-12-01 19:35:30 +0800

    progressbar changed

--- app/src/main/java/com/example/yzy/imageartist/Color.kt
+++ app/src/main/java/com/example/yzy/imageartist/Color.kt
@@ -3,15 +3,26 @@ package com.example.yzy.imageartist
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
+import org.jetbrains.anko.find
 
 class Color : AppCompatActivity() {
     public lateinit var mPhoto: ImageView
-
+    private lateinit var mText1: TextView
+    private lateinit var mText2: TextView
+    private lateinit var mText3: TextView
+    private lateinit var mText4: TextView
+    private lateinit var mText5: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_color)
         this.setTitle(R.string.choose_color)
         mPhoto=findViewById(R.id.color_photo)
+        mText1=findViewById(R.id.text_one)
+        mText2=findViewById(R.id.text_two)
+        mText3=findViewById(R.id.text_three)
+        mText4=findViewById(R.id.text_four)
+        mText5=findViewById(R.id.text_five)
         WorkspaceManager.bitmap?.let {
             mPhoto.setImageBitmap(it)
         }
