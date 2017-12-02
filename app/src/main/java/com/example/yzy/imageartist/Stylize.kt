e99bd9c12f179ebd59ca5ca33234bad9c74df583
yzyDavid <yzyDavid@qq.com>
2017-12-03 02:20:49 +0800

    added menu items.

--- app/src/main/java/com/example/yzy/imageartist/Stylize.kt
+++ app/src/main/java/com/example/yzy/imageartist/Stylize.kt
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Base64
 import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.view.Window
 import android.widget.Button
@@ -33,6 +34,11 @@ class Stylize : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         super.onCreateOptionsMenu(menu)
         menuInflater.inflate(R.menu.menu_style, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        super.onOptionsItemSelected(item)
         return true
     }
 }
