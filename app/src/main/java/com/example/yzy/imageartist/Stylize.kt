94d16f1283b3ea11f22b18ece0ce2d4b89c08600
yzyDavid <yzyDavid@qq.com>
2017-12-03 00:49:23 +0800

    added menu, empty.

--- app/src/main/java/com/example/yzy/imageartist/Stylize.kt
+++ app/src/main/java/com/example/yzy/imageartist/Stylize.kt
@@ -3,6 +3,7 @@ package com.example.yzy.imageartist
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Base64
+import android.view.Menu
 import android.view.View
 import android.view.Window
 import android.widget.Button
@@ -15,25 +16,23 @@ import retrofit2.Retrofit
 
 class Stylize : AppCompatActivity() {
 
-    private lateinit var mButtonHello: Button
-    public lateinit var mPhoto: ImageView
-    //private val stylizeModel = StylizeModel(this)
+    lateinit var mPhoto: ImageView
+    // private val stylizeModel = StylizeModel(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_stylize)
         this.setTitle(R.string.stylize)
-
         mPhoto = findViewById(R.id.stylize_photo)
         WorkspaceManager.bitmap?.let {
             mPhoto.setImageBitmap(it)
         }
     }
 
-    fun stylizeShow(view: View) {
-        mButtonHello.setOnClickListener {
-            //stylizeModel.getHello()
-        }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.menu_style, menu)
+        return true
     }
 }
