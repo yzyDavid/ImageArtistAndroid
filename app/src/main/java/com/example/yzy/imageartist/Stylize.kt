f250cd460d33c958810e7dd1a7e77a19827ed41b
wangtianyuanpanda <973920187@qq.com>
2017-12-03 22:55:53 +0800

    choose picture button in stylize.kt added

--- app/src/main/java/com/example/yzy/imageartist/Stylize.kt
+++ app/src/main/java/com/example/yzy/imageartist/Stylize.kt
@@ -1,7 +1,10 @@
 package com.example.yzy.imageartist
 
+import android.app.Activity
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
 import android.util.Base64
 import android.view.View
 import android.widget.Button
@@ -13,9 +16,11 @@ import retrofit2.Response
 import retrofit2.Retrofit
 
 class Stylize : AppCompatActivity() {
-
+    private val PICTURE: Int = 0
     private lateinit var mButtonHello: Button
     public lateinit var mPhoto: ImageView
+    private lateinit var mChoosepicButton: FloatingActionButton
+    private val galleryModel = GalleryModel(this, PICTURE)
     //private val stylizeModel = StylizeModel(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,6 +31,10 @@ class Stylize : AppCompatActivity() {
         WorkspaceManager.bitmap?.let {
             mPhoto.setImageBitmap(it)
         }
+        mChoosepicButton = findViewById(R.id.choosepic_button)
+        mChoosepicButton.setOnClickListener{
+            galleryModel.startGallery()
+        }
     }
 
     fun stylizeShow(view: View) {
@@ -33,4 +42,16 @@ class Stylize : AppCompatActivity() {
             //stylizeModel.getHello()
         }
     }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode != Activity.RESULT_OK) {
+            return
+        }
+
+        when (requestCode) {
+            PICTURE -> WorkspaceManager.style = galleryModel.getBitmap(resultCode, data!!)
+        }
+
+    }
+
 }
