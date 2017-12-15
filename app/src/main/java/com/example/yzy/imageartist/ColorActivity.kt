e31d5b9e697a61168d533368e4a173c04e829bd8
yzyDavid <yzyDavid@qq.com>
2017-12-15 22:39:25 +0800

    fix View to make, in ColorActivity.

--- app/src/main/java/com/example/yzy/imageartist/ColorActivity.kt
+++ app/src/main/java/com/example/yzy/imageartist/ColorActivity.kt
@@ -2,10 +2,14 @@ package com.example.yzy.imageartist
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.annotation.IdRes
+import android.util.Log
 import android.widget.ImageView
+import android.widget.TextView
 
 class ColorActivity : AppCompatActivity() {
-    public lateinit var mPhoto: ImageView
+    lateinit var mPhoto: ImageView
+    lateinit var model: ColorModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,5 +19,20 @@ class ColorActivity : AppCompatActivity() {
         WorkspaceManager.bitmap?.let {
             mPhoto.setImageBitmap(it)
         }
+        model = ColorModel(this)
+        listOf(
+                Pair(1, R.id.color_1),
+                Pair(2, R.id.color_2),
+                Pair(3, R.id.color_3),
+                Pair(4, R.id.color_4),
+                Pair(5, R.id.color_5))
+                .map { Pair(it.first, findViewById<TextView>(it.second)) }
+                .forEach { pair ->
+                    pair.second.setOnClickListener {
+                        WorkspaceManager.bitmap?.let {
+                            model.getThemeColor(it, pair.first)
+                        }
+                    }
+                }
     }
 }
