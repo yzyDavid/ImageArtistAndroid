e9ce73ad8f27a4b5feec16bf2cdff48adc201f40
poiiiicen <poiiiicen@gmail.com>
2017-12-15 10:14:37 +0800

    Create FrameModel

---
+++ app/src/main/java/com/example/yzy/imageartist/FrameModel.kt
@@ -0,0 +1,19 @@
+package com.example.yzy.imageartist
+
+import android.graphics.Bitmap
+import org.opencv.android.Utils
+import org.opencv.core.CvType
+import org.opencv.core.Mat
+import org.opencv.core.Size
+import org.opencv.imgproc.Imgproc
+
+class FrameModel(private val activiy: Frame) {
+    fun appendFrame(image: Bitmap, frame: Bitmap) {
+        val matImage = Mat(image.height, image.width, CvType.CV_8UC4)
+        Utils.bitmapToMat(image, matImage)
+        val matFrame = Mat(frame.height, frame.width, CvType.CV_8UC4)
+        Utils.bitmapToMat(frame, matFrame)
+        Imgproc.resize(matFrame, matFrame, Size(image.width.toDouble(), image.height.toDouble()))
+
+    }
+}
