f318d7921753c4ef8846c88f06039a1535cd97bb
yzyDavid <yzyDavid@qq.com>
2017-12-15 20:05:40 +0800

    added JvmStatic to Utils fun.

--- app/src/main/java/com/example/yzy/imageartist/Utils.kt
+++ app/src/main/java/com/example/yzy/imageartist/Utils.kt
@@ -12,6 +12,7 @@ import org.opencv.core.Scalar
  * Created by yzy on 12/11/17.
  */
 object Utils {
+    @JvmStatic
     public fun fromBitmapToMat(image: Bitmap): Mat {
         val mat = Mat(image.height, image.width, CvType.CV_8U, Scalar(4.toDouble()))
         val tmpBitmap = image.copy(Bitmap.Config.ARGB_8888, true)
@@ -19,16 +20,19 @@ object Utils {
         return mat
     }
 
+    @JvmStatic
     public fun fromMatToBitmap(mat: Mat): Bitmap {
         val result = Bitmap.createBitmap(mat.cols(), mat.rows(), Bitmap.Config.ARGB_8888)
         CvUtils.matToBitmap(mat, result)
         return result
     }
 
+    @JvmStatic
     public fun Bitmap.toMat(): Mat {
         return fromBitmapToMat(this)
     }
 
+    @JvmStatic
     public fun Mat.toBitmap(): Bitmap {
         return fromMatToBitmap(this)
     }
