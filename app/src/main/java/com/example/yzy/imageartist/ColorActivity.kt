57c3a28ce884e81b32be4132442621dadad5be14
yzyDavid <yzyDavid@qq.com>
2017-12-18 23:25:48 +0800

    finish ProgressBar blocking the commit.

--- app/src/main/java/com/example/yzy/imageartist/ColorActivity.kt
+++ app/src/main/java/com/example/yzy/imageartist/ColorActivity.kt
@@ -4,18 +4,24 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.annotation.IdRes
 import android.util.Log
+import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import me.zhanghai.android.materialprogressbar.MaterialProgressBar
 
 class ColorActivity : AppCompatActivity() {
     lateinit var mPhoto: ImageView
     private lateinit var model: ColorModel
+    lateinit var mpb: MaterialProgressBar
+    var isWaiting = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_color)
         this.setTitle(R.string.choose_color)
         mPhoto = findViewById(R.id.color_photo)
+        mpb = findViewById(R.id.color_mpb)
+        mpb.visibility = View.INVISIBLE
         WorkspaceManager.bitmap?.let {
             mPhoto.setImageBitmap(it)
         }
@@ -30,9 +36,13 @@ class ColorActivity : AppCompatActivity() {
                 .forEach { pair ->
                     pair.second.setOnClickListener {
                         WorkspaceManager.bitmap?.let {
+                            if (isWaiting) {
+                                return@setOnClickListener
+                            }
                             Log.i("Color", "start theme coloring")
+                            isWaiting = true
+                            mpb.visibility = View.VISIBLE
                             model.getThemeColor(it, pair.first)
-                            Log.i("Color", "finish theme coloring")
                         }
                     }
                 }
