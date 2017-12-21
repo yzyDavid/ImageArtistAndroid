ed490b7c795298696a77bf08a4b1405802e707bb
poiiiicen <poiiiicen@gmail.com>
2017-12-22 02:23:57 +0800

    Draw graffiti but not continuous

--- app/src/main/java/com/example/yzy/imageartist/GraffitiActivity.kt
+++ app/src/main/java/com/example/yzy/imageartist/GraffitiActivity.kt
@@ -1,6 +1,8 @@
 package com.example.yzy.imageartist
 
 import android.annotation.SuppressLint
+import android.graphics.Bitmap
+import android.graphics.Color
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
@@ -8,9 +10,11 @@ import android.util.Log
 import android.view.GestureDetector
 import android.view.MotionEvent
 import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.RelativeLayout
 import android.widget.Toast
+import org.jetbrains.anko.find
 
 
 class GraffitiActivity : AppCompatActivity() {
@@ -26,6 +30,12 @@ class GraffitiActivity : AppCompatActivity() {
     private var mode = NOTHING
     private lateinit var mPhoto: ImageView
     private lateinit var mLayout: RelativeLayout
+    private lateinit var mButtonRed: ImageView
+    private lateinit var mButtonBlue: ImageView
+    private lateinit var mButtonGreen: ImageView
+    private lateinit var mButtonGray: ImageView
+    private lateinit var mButtonWhite: ImageView
+    private val image = Bitmap.createBitmap(WorkspaceManager.bitmap!!.width, WorkspaceManager.bitmap!!.height, Bitmap.Config.ARGB_8888)
     private var mLayoutWidth = 0.0
     private var mLayoutHeight = 0.0
     private var picWidth = 0.0
@@ -34,18 +44,31 @@ class GraffitiActivity : AppCompatActivity() {
     private var ratio = 0.0
     private var imgWidth = 0.0
     private var imgHeight = 0.0
+    private var circle = 20
 
-    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_graffiti)
         this.setTitle(R.string.graffiti)
         mPhoto = findViewById(R.id.graffiti_photo)
         mLayout = findViewById(R.id.graffiti_photo_area)
+        mButtonRed = findViewById(R.id.color_red)
+        mButtonBlue = findViewById(R.id.color_blue)
+        mButtonGreen = findViewById(R.id.color_green)
+        mButtonGray = findViewById(R.id.color_gray)
+        mButtonWhite = findViewById(R.id.color_white)
+        mButtonRed.setOnClickListener { mode = RED }
+        mButtonBlue.setOnClickListener { mode = BLUE }
+        mButtonGreen.setOnClickListener { mode = GREEN }
+        mButtonGray.setOnClickListener { mode = GRAY }
+        mButtonWhite.setOnClickListener { mode = WHITE }
         WorkspaceManager.bitmap?.let {
             mPhoto.setImageBitmap(it)
         }
-
+        for (i in 0 until WorkspaceManager.bitmap!!.width)
+            for (j in 0 until WorkspaceManager.bitmap!!.height) {
+                image.setPixel(i, j, WorkspaceManager.bitmap!!.getPixel(i, j))
+            }
         /*
         mLayout.setOnTouchListener { view, motion ->
             view.performClick()
@@ -56,9 +79,14 @@ class GraffitiActivity : AppCompatActivity() {
 
         mLayout.setOnTouchListener { view, motion ->
             when (motion.action) {
-                MotionEvent.ACTION_MOVE -> drawGraffiti(view, motion)
+                MotionEvent.ACTION_DOWN -> return@setOnTouchListener true
+                MotionEvent.ACTION_MOVE -> {
+                    drawGraffiti(view, motion)
+                    return@setOnTouchListener true
+                }
+                else -> return@setOnTouchListener false
             }
-            return@setOnTouchListener true
+            return@setOnTouchListener false
         }
     }
 
@@ -96,8 +124,28 @@ class GraffitiActivity : AppCompatActivity() {
                 imgWidth = picWidth / ratio
                 imgHeight = mLayoutHeight
             }
+            circle = (circle * ratio).toInt()
         }
-        val x = motion.x
-        val y = motion.y
+        if (mode == NOTHING) return
+        val x = (motion.x - mLayoutPos[0] - (mLayoutWidth - imgWidth) / 2) * ratio
+        val y = (motion.y - mLayoutPos[1] - (mLayoutHeight - imgHeight) / 2) * ratio
+        var i: Int
+        var j: Int
+        //val image = Bitmap.createBitmap(WorkspaceManager.bitmap!!.width, WorkspaceManager.bitmap!!.height, Bitmap.Config.ARGB_8888)
+        for (i in -circle..circle) {
+            (-circle..circle)
+                    .filter { x + i in 0.0..picWidth && y + it in 0.0..picHeight && i * i + it * it <= circle * circle }
+                    .forEach {
+                        image.setPixel((x + i).toInt(), (y + it).toInt(), when (mode) {
+                            RED -> Color.RED
+                            BLUE -> Color.BLUE
+                            GREEN -> Color.GREEN
+                            GRAY -> Color.GRAY
+                            WHITE -> Color.WHITE
+                            else -> WorkspaceManager.bitmap!!.getPixel((x + i).toInt(), (y + it).toInt())
+                        })
+                    }
+        }
+        mPhoto.setImageBitmap(image)
     }
 }
